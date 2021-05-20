684496766ca2ad9549229441f3646830398e4838
fahjin <yongfahjin@hotmail.com>
2021-05-20 20:37:06 +0800

    1st task

--- App.js
+++ App.js
@@ -1,12 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity,buttonPressed } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Bus arrival time:</Text>
+      <Text style={styles.loadingText}>Loading...</Text>
       <StatusBar style="auto" />
+      <TouchableOpacity onPress={buttonPressed}
+                        style={styles.button}>
+
+        <Text style={styles.buttonText}>
+          Refresh!
+        </Text>
+      </TouchableOpacity>
+
+      <StatusBar style="auto"/>
+
     </View>
   );
 }
@@ -18,4 +29,22 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  button: {
+    backgroundColor: 'green',
+    padding: 20,
+    borderRadius: 10,
+    marginTop: 20
+  },
+
+  loadingText: {
+    fontWeight: 'bold',
+    fontSize: 40,
+  },
+
+  buttonText: {
+    color: 'white',
+    fontWeight: 'bold',
+    fontSize: 40,
+  },
+
 });
